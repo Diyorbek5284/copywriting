a3cfa659c165a1c742c51ef20b350eb6e5ae45d9
Sobirov Diyorbek <diyorsobirov976@gmail.com>
2023-05-27 20:31:55 +0500

    final

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+/** navbar reponsive bo;ganda ishga tushadigan qodi **/
+
+const navToggleBtn = document.querySelector("[data-nav-toggle-btn]");
+const header = document.querySelector("[data-header]");
+
+navToggleBtn.addEventListener("click", function () {
+  header.classList.toggle("active");
+});
